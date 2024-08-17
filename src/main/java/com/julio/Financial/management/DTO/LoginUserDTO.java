cb03177ac64d03a888b1cefe9a9a1869d6f44870
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-17 01:10:12 -0300

    feat: RegisterUserDTO e LoginUserDTO

---
+++ src/main/java/com/julio/Financial/management/DTO/LoginUserDTO.java
@@ -0,0 +1,4 @@
+package com.julio.Financial.management.DTO;
+
+public record LoginUserDTO(String email, String password) {
+}
---
+++ src/main/java/com/julio/Financial/management/DTO/RegisterUserDTO.java
@@ -0,0 +1,4 @@
+package com.julio.Financial.management.DTO;
+
+public record RegisterUserDTO(String firstName, String lastName, String email, String password) {
+}
