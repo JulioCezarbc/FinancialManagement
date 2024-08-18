8e4674d30572267edcf80a027ee001d863a849c0
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-08-18 12:40:55 -0300

    feat: Adicionado a função de buscar por EMAIL

--- src/main/java/com/julio/Financial/management/repository/UserRepository.java
+++ src/main/java/com/julio/Financial/management/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.julio.Financial.management.repository;
 import com.julio.Financial.management.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User,UUID> {
+    Optional<User> findByEmail(String email);
 }
